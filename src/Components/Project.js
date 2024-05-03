7dc707a6e549d88c43e963411c7aa70694c24b2d
felanadiamondra <felana.diamondra.randriamadison@esti.mg>
2024-05-03 15:16:37 +0300

    Project video

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { data } from './mockData';
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+import milvusvid from "../data/milvus.mp4";
 
 function Project() {
   const slideLeft = () => {
@@ -14,10 +15,7 @@ function Project() {
     slider.scrollLeft = slider.scrollLeft + 500;
   };
 
-  return (
-    <div className='mx-20 ml-40 p-10'>
-      <h1 className="text-center mt-10 font-bold text-blue_picton">My project</h1>
-      <div className='relative flex items-center'>
+     {/* <div className='relative flex items-center'>
         <MdChevronLeft className='opacity-50 cursor-pointer hover:opacity-100' onClick={slideLeft} size={40} />
         <div
           id='slider'
@@ -30,6 +28,30 @@ function Project() {
           ))}
         </div>
         <MdChevronRight className='opacity-50 cursor-pointer hover:opacity-100' onClick={slideRight} size={40} />
+      </div> */}
+
+  return (
+    <div className='mx-20 ml-40 p-10'>
+      <h1 className="text-center mt-10 font-bold text-blue_picton">My project</h1>
+     
+      <div className="flex flex-col md:flex-row mt-5">
+        {/* Partie vidéo */}
+        <div className="w-full md:w-2/3 lg:w-3/4 flex justify-center">
+          <video
+            className="w-full"
+            src={milvusvid}
+            controls
+          />
+        </div>
+
+        {/* Partie description */}
+        <div className="w-full md:w-1/3 lg:w-1/4 p-5 md:p-10">
+          <p className='text-center'>
+            In a world where travel and exploration are becoming increasingly important, innovation plays a crucial role in enhancing the traveler's experience. 
+            One of the most exciting advances in this field is the use of artificial intelligence to facilitate the search for places, attractions and experiences.
+            Smartour is an application that revolutionizes travel exploration using AI with the Milvus database.
+          </p>
+        </div>
       </div>
     </div>
   );
